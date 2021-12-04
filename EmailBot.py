1d8e118d1737113c30998948270a1fc44c181e4c
Prasanta <acharyya.prasanta05@gmail.com>
2021-12-04 14:21:41 +0530

    first commit

--- EmailBot.py
+++ EmailBot.py
@@ -6,11 +6,13 @@ print("Enter Your Password : \n")
 passw = input()
 print("Enter receiver's mail ID : ")
 sml = input()
+print("Enter message :")
+msg = input()
 #SMTP server connection
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
 server.login(mo, passw)
-server.sendmail(mo, sml, "Hi there, how's your day going?")
+server.sendmail(mo, sml, msg)
 
 
 
